db0378114a4ad023100f432f4218b3495ed169f6
mintsomething <git@mintsomething.com>
2024-02-03 00:49:13 -0500

    images, instagram, link fix

--- src/views/Home/components/LockedSwitchCard/index.tsx
+++ src/views/Home/components/LockedSwitchCard/index.tsx
@@ -4,11 +4,11 @@ import LockedSwitchIcon from "@/components/Icons/SwitchLockIcon";
 const LockedSwitchCard = () => {
   return (
     <div className="__card flex flex-col relative justify-center items-center p-[14px] gap-1 text-center">
-      <h2 className="__title_lg">Webstore</h2>
-      <p className="__text_sm">Open</p>
+      <h2 className="__title_lg">Online Store</h2>
+      <p className="__text_sm"></p>
 
       <a
-        href="store.fitfolabs.com"
+        href="https://store.fitfolabs.com/"
         target="_blank"
         className="__btn_rounded absolute bottom-[14px] left-[14px]"
       >
