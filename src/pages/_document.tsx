a60be2a6b7a1a34b6450e453632b61bb5774f0a4
sadekxD <sadekirfan3@gmail.com>
2024-06-18 17:07:17 +0400

    fix: overflow issue in body

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,15 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <title>Fitfo Labs</title>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang="en">
+			<Head>
+				<title>Fitfo Labs</title>
+			</Head>
+			<body style={{ overflowX: "hidden" }}>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
